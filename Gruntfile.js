596deb0796109dffc430d42c3e5725e4d4704c41
Christopher Luna <dev@christopher-luna.com>
2014-05-27 22:35:46 -0500

    Agregado grunt. Checa los archivos con jshint.

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+/* jslint node: true */
+"use strict";
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg     : grunt.file.readJSON('package.json'),
+    jshint  : {
+      all     : ['package.json', 'bower.json', 'curp.js', 'Gruntfile.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', [
+    'jshint'
+  ]);
+
+};
+
