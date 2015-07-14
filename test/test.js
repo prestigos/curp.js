2347eee022de9fc83b8c7d3cac1b56b5e9e82822
Dudemullet <elpaip@gmail.com>
2015-07-14 18:15:57 -0500

    Agrega pruebas para las posiciones 1-4

---
+++ test/test.js
@@ -0,0 +1,87 @@
+var assert = require('assert');
+var curp = require('./../curp.js');
+
+var persona = new Persona();
+beforeEach(function() {
+  persona.nombre= "pepito";
+  persona.apellido_paterno= "perez";
+  persona.apellido_materno= "alvarado";
+  persona.sexo= "M";
+  persona.estado= "DF";
+  persona.fecha_nacimiento= [01, 01, 1969];
+});
+
+describe('Posiciones 1-4', function() {
+  it('Deberia regresar "x" si la primer letra de algun de los nombres es Ñ', function() {
+    persona.setName('alberto', 'ñando', 'rodriguez');
+    var letras = curp(persona).substr(0,4);
+    // persona.toString();
+    assert.equal(letras, 'XARA');
+  });
+
+  describe('Deberia utilizar la primera letra de la primera palabra cuando el nombre es compuesto. Siempre y cuando no sea "MARIA, MA, MA., JOSE, J o JJ" ', function() {
+    it('Para Maria', function(){
+      persona.setName('maria luisa', 'perez', 'hernandez');
+      var letras = curp(persona).substr(0,4);
+      // persona.toString();
+      assert.equal(letras, 'PEHL');
+    });
+
+    it('Para compuesto regular', function(){
+      persona.setName('luis enrique', 'romero', 'palazuelos')
+      letras = curp(persona).substr(0,4);
+      // persona.toString();
+      assert.equal(letras, 'ROPL');
+    })
+  });
+
+  it('Deberia regresar "X" en caso que un caracter especial como "/" o "-" vaya a ser utilizado en la creacion de la clave', function(){
+    persona.setName("juan jose", "d/amico", "alvarez");
+    var letras = curp(persona).substr(0,4);
+    // persona.toString();
+    assert.equal(letras, 'DXAJ');
+  });
+
+  it("Deberia remover dieresis si se llegara a tener que utilizar una letra con ellas", function(){
+    persona.setName('Pedro', 'güero', 'hernandez');
+    var letras = curp(persona).substr(0,4);
+    assert.equal(letras, 'GUHP');
+  });
+
+  it('Deberia utilizar la primera palabra en apellidos compuestos', function(){
+    persona.setName('rocio', 'riva palacio', 'cruz');
+    var letras = curp(persona).substr(0,4);
+    assert.equal(letras, 'RICR');
+  });
+
+  it('Deberia de quitar las preposiciones, conjunciones o contracciones', function(){
+    persona.setName('carlos', 'MC Gregor', 'lopez');
+    var letras = curp(persona).substr(0,4);
+    assert.equal(letras, 'GELC');
+  });
+
+  it('Deberia cambiar la segunda letra por una "X" si la clave es una palabra altisonante', function(){
+    persona.setName('ofelia', 'pedrero', 'dominguez');
+    var letras  = curp(persona).substr(0,4);
+    assert.equal(letras, 'PXDO');
+  });
+
+  it('Deberia poner una "x" en vez de una segunda letra si el primer apellido no tiene una vocal interna', function(){
+    persona.setName('andres', 'ich', 'rodríguez');
+    var letras = curp(persona).substr(0,4);
+    assert.equal(letras, 'IXRA');
+  });
+
+  it('Debera asignar una x en la 3ra posicion de la clave si no hay segundo apellido', function(){
+    persona.setName('luis', 'perez', '');
+    var letras = curp(persona).substr(0,4);
+    assert.equal(letras, 'PEXL');
+  });
+});
+
+function Persona() {};
+Persona.prototype.setName = function(nombre, apellido_paterno, apellido_materno){
+  this.nombre = nombre;
+  this.apellido_paterno = apellido_paterno;
+  this.apellido_materno = apellido_materno;
+};
